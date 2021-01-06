2b36ae72d28ec2934857ccceb5097cfcce7a0d39
Jepama77 <68938258+Jepama77@users.noreply.github.com>
2021-01-06 08:39:20 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,17 @@ radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 2) {
         basic.showIcon(IconNames.Angry)
         music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
+    } else {
+        basic.showIcon(IconNames.Yes)
     }
 })
 input.onButtonPressed(Button.A, function () {
+    valore = randint(1, 3)
+    radio.sendNumber(valore)
+    basic.showString("INVIO")
+})
+input.onButtonPressed(Button.B, function () {
     basic.clearScreen()
 })
-let valore = randint(0, 10)
-radio.sendNumber(valore)
+let valore = 0
+basic.showString("GO")
